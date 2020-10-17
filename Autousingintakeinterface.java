cb9415802d892a10f5cd49a2ea6aa80c8bb65b57
obsidian5 <55633923+obsidian5@users.noreply.github.com>
2020-10-17 16:22:48 -0700

    Add files via upload

---
+++ Autousingintakeinterface.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+
+public interface Autousingintakeinterface {
+   //start intake method
+    public void startIntake(int speed, int time);
+  //start outake method
+    public void startOutake(int speed, int time);
+
+}
