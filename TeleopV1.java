d3992dc66458acf62f7f82702c88de7e86945f60
AkhileshBasetty <68478953+AkhileshBasetty@users.noreply.github.com>
2020-11-25 13:28:04 -0800

    Update TeleopV1.java
    changed encoder values for wobble goal arm; added run without encoders

--- TeleopV1.java
+++ TeleopV1.java
@@ -39,6 +39,11 @@ public class TeleopV1 extends LinearOpMode {
         robot.FrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         robot.BackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         robot.BackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        
+        robot.FrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.FrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.BackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.BackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         robot.Wgoalarm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
@@ -46,7 +51,7 @@ public class TeleopV1 extends LinearOpMode {
         
         int WA_pos_1 = original_WA_pos;
         int WA_pos_2 = original_WA_pos + 500;
-        int WA_pos_3 = original_WA_pos + 1600;
+        int WA_pos_3 = original_WA_pos + 1400;
 
         double wobble_goal_arm_pos = 1;
         double WAerror=0;
