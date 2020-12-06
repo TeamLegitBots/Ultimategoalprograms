8c60bb5e2a90a2c5aac2a48eaad52dd64cbfc46b
AkhileshBasetty <68478953+AkhileshBasetty@users.noreply.github.com>
2020-12-05 22:12:34 -0800

    Update TeleopV1.java
    fixed the power shot shooter speed (it was setting it at 2 different speeds if both the left bumper and the left stick were pressed)

--- TeleopV1.java
+++ TeleopV1.java
@@ -9,14 +9,15 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.teamcode.HardwareMap.LegitbotV1;
 
 
 @TeleOp(name = "MyTeleop")
 //@Disabled
 public class TeleopV1 extends LinearOpMode {
 
-    Hardwaremap robot = new Hardwaremap();
-    
+    LegitbotV1 robot = new LegitbotV1();
+
     double sensitivity = .5;
 
 
@@ -26,17 +27,17 @@ public class TeleopV1 extends LinearOpMode {
 
 
         robot.init(hardwareMap);
-        
+
         robot.WheelOutake.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
         robot.Wgoalarm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        
-       
-        
+
+
+
         robot.FrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         robot.FrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         robot.BackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         robot.BackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        
+
         robot.FrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         robot.FrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         robot.BackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -45,7 +46,7 @@ public class TeleopV1 extends LinearOpMode {
         robot.Wgoalarm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         int original_WA_pos = robot.Wgoalarm.getCurrentPosition();
-        
+
         int WA_pos_1 = original_WA_pos;
         int WA_pos_2 = original_WA_pos + 500;
         int WA_pos_3 = original_WA_pos + 1400;
@@ -68,7 +69,7 @@ public class TeleopV1 extends LinearOpMode {
             float strafe = gamepad1.left_stick_x;
             float turn = gamepad1.left_stick_y;
             */
-            
+
             float intake = gamepad2.right_stick_x;
             float wheelouttake = gamepad2.right_stick_y;
             float outtake = gamepad2.left_stick_y;
@@ -79,12 +80,12 @@ public class TeleopV1 extends LinearOpMode {
             float BR = drive - turn + strafe;
             float BL = -drive  - turn - strafe;
             */
-            
-            
-            
+
+
+
             robot.Intake.setPower(-intake);
             //robot.WheelOutake.setPower(0.80 * (outtake));
-            
+
             robot.Pulley.setPower(intake);
             /*
             robot.FrontRight.setPower(sensitivity * FR);
@@ -94,15 +95,14 @@ public class TeleopV1 extends LinearOpMode {
             */
             setDriveMotorPower();
 
-
-            if (outtake<-.1){
+            
+            if (gamepad2.left_bumper && outtake<-.1){
+                robot.WheelOutake.setPower(powershotspeed);
+            } else if(outtake<-.1){
                 robot.WheelOutake.setPower(shooterspeed);
             } else{
                 robot.WheelOutake.setPower(0);
 
-            }
-            if (gamepad2.left_bumper && outtake<-.1){
-                robot.WheelOutake.setPower(powershotspeed);
             }
             //automatic ringgate
             /*if ((outtake<-.1 && intake>.1) || (intake<-.1 )){
@@ -114,7 +114,7 @@ public class TeleopV1 extends LinearOpMode {
         
             
             */
-            
+
             //manual ringgate
             if (gamepad2.dpad_right){
                 robot.Ring_gate.setPosition(1);
@@ -130,7 +130,7 @@ public class TeleopV1 extends LinearOpMode {
             if (gamepad2.b && wobble_goal_arm_pos !=1){
                 robot.Wgoalservo.setPosition(1);
             }
-            
+
             //drop intake
             if (gamepad2.y){
                 robot.Backservo.setPosition(.25);
@@ -140,9 +140,9 @@ public class TeleopV1 extends LinearOpMode {
             }
 
 
-           
+
             // Drag the sparkmini system down
-            
+
 
             // Stop if not linear motion kit input
             if(gamepad1.left_trigger == 0 && gamepad1.right_trigger == 0) {
@@ -150,20 +150,20 @@ public class TeleopV1 extends LinearOpMode {
 
             }
 
-         
-            
 
-           
 
-        
+
+
+
+
             //wobble goal arm
-            
+
             if(gamepad2.dpad_up && wobble_goal_arm_pos<3){
                 wobble_goal_arm_pos = wobble_goal_arm_pos+1;
                 sleep(500);
             }
             if(gamepad2.dpad_down && wobble_goal_arm_pos>1){
-                wobble_goal_arm_pos = wobble_goal_arm_pos-1; 
+                wobble_goal_arm_pos = wobble_goal_arm_pos-1;
                 sleep(500);
 
             }
@@ -189,7 +189,7 @@ public class TeleopV1 extends LinearOpMode {
                 robot.Wgoalarm.setPower(0);
                 robot.Wgoalarm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-            } 
+            }
             //telemetry.addData("target position",wobble_goal_arm_pos);
             //telemetry.addData("current position",robot.Wgoalarm.getCurrentPosition());
             //telemetry.update();
@@ -197,14 +197,14 @@ public class TeleopV1 extends LinearOpMode {
             if (wobble_goal_arm_pos == 2 && (robot.Wgoalarm.getCurrentPosition() < WA_pos_2 - WAerror || robot.Wgoalarm.getCurrentPosition() > WA_pos_2 + WAerror )){
                 robot.Wgoalarm.setTargetPosition(WA_pos_2);
                 robot.Wgoalarm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                
+
 
                 if (robot.Wgoalarm.getCurrentPosition()>WA_pos_2){
                     robot.Wgoalarm.setPower(-1);
                 } else {
                     robot.Wgoalarm.setPower(1);
                 }
-            
+
                 while(robot.Wgoalarm.isBusy() && opModeIsActive()){
                     setDriveMotorPower();
                     if (gamepad2.dpad_up || gamepad2.dpad_down || gamepad2.a || gamepad2.b){
@@ -214,7 +214,7 @@ public class TeleopV1 extends LinearOpMode {
                 robot.Wgoalarm.setPower(0);
                 robot.Wgoalarm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-            } 
+            }
             //move Wgoal arm to position 3
             if (wobble_goal_arm_pos == 3 && (robot.Wgoalarm.getCurrentPosition() < WA_pos_3 - WAerror || robot.Wgoalarm.getCurrentPosition() > WA_pos_3 + WAerror) ){
                 robot.Wgoalarm.setTargetPosition(WA_pos_3);
@@ -233,12 +233,12 @@ public class TeleopV1 extends LinearOpMode {
                 robot.Wgoalarm.setPower(0);
                 robot.Wgoalarm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             }
-            
+
             //sensitivity
             if(gamepad1.dpad_up && sensitivity<1){
                 while(gamepad1.dpad_up){}
                 sensitivity = sensitivity + .1;
-                
+
             }
             if(gamepad1.dpad_down && sensitivity>.1){
                 while(gamepad1.dpad_down){}
@@ -247,24 +247,24 @@ public class TeleopV1 extends LinearOpMode {
             }
             telemetry.addData("sensitivity: ", sensitivity);
             telemetry.update();
-            
 
-            }             
-            
-        }
-        public void setDriveMotorPower(){      
-            float drive = -gamepad1.right_stick_x;
-            float strafe = gamepad1.left_stick_x;
-            float turn = gamepad1.left_stick_y;
 
-            float FR = drive - turn - strafe;
-            float FL = -drive - turn  + strafe;
-            float BR = drive - turn + strafe;
-            float BL = -drive  - turn - strafe;
-            
-            robot.FrontRight.setPower(sensitivity * FR);
-            robot.FrontLeft.setPower(sensitivity * FL );
-            robot.BackRight.setPower(sensitivity * BR);
-            robot.BackLeft.setPower(sensitivity * BL);
         }
+
+    }
+    public void setDriveMotorPower(){
+        float drive = -gamepad1.right_stick_x;
+        float strafe = gamepad1.left_stick_x;
+        float turn = gamepad1.left_stick_y;
+
+        float FR = drive - turn - strafe;
+        float FL = -drive - turn  + strafe;
+        float BR = drive - turn + strafe;
+        float BL = -drive  - turn - strafe;
+
+        robot.FrontRight.setPower(sensitivity * FR);
+        robot.FrontLeft.setPower(sensitivity * FL );
+        robot.BackRight.setPower(sensitivity * BR);
+        robot.BackLeft.setPower(sensitivity * BL);
+    }
 }
