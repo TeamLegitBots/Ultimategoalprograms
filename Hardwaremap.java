2b8075d99f50cda6708a16fb44ca0c40769d776d
AkhileshBasetty <68478953+AkhileshBasetty@users.noreply.github.com>
2020-12-06 16:25:07 -0800

    Update Hardwaremap.java
    changed the circumference value

--- Hardwaremap.java
+++ Hardwaremap.java
@@ -75,7 +75,7 @@ public class LegitbotV1 {
     static final double WHEEL_DIAMETER_INCHES = 1.49606;     // For figuring circumference
     static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
             (WHEEL_DIAMETER_INCHES * 3.1415);
-    static final double Circumference = 40.6;
+    static final double Circumference = 40.53;
 
 
 
@@ -154,6 +154,7 @@ public class LegitbotV1 {
         FrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
 
 
     }
