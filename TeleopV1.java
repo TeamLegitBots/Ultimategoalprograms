274b3054d7bdc99f9dacedbb8a1fa062d2cbc852
AkhileshBasetty <68478953+AkhileshBasetty@users.noreply.github.com>
2021-01-11 22:29:04 -0800

    Update TeleopV1.java
    WG arm starts off at position 2 like it does at the end of autonomous

--- TeleopV1.java
+++ TeleopV1.java
@@ -47,9 +47,9 @@ public class TeleopV1 extends LinearOpMode {
 
         int original_WA_pos = robot.Wgoalarm.getCurrentPosition();
 
-        int WA_pos_1 = original_WA_pos;
-        int WA_pos_2 = original_WA_pos + 450;
-        int WA_pos_3 = original_WA_pos + 1500;
+        int WA_pos_1 = original_WA_pos-450;
+        int WA_pos_2 = original_WA_pos;
+        int WA_pos_3 = original_WA_pos + 1500-450;
 
         double wobble_goal_arm_pos = 1;
         double WAerror=0;
